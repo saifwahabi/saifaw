a33c86139a91e2f24692c96c108d0baaf1361417
saifwahabi <128902937+saifwahabi@users.noreply.github.com>
2024-08-14 17:29:34 +0100

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,8 +84,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lXYdyMpJPiQEeERQxPxELthYQMZxhlBj',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '32353',
     }
 }
 
