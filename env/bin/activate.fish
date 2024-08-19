d298de5f41fb45c7ba4074a7970c627b69452a66
saifwahabi <asmrcoding40@gmail.com>
2024-08-20 00:03:15 +0100

    initial commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/saifaw/Documents/portfolio/env"
+set -gx VIRTUAL_ENV "/home/saifaw/Documents/saifaw/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
