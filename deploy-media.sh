3cdf9b3b1c1b2611bf865b81799a68a32e133b45
saifwahabi <asmrcoding40@gmail.com>
2024-08-22 16:34:49 +0100

    initial commit

---
+++ deploy-media.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ./media
+git add .
+git commit -m "initial commit"
+git push -u origin main
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
+
+MEDIA_URL = 'https://github.com/saifwahabi/saifaw_media'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
